7705010359f603bbd354400ec5007f573a413fba
uchy <chikito960707@gmail.com>
2020-05-18 01:04:00 +0900

    first commit

---
+++ Atcoder/atcoder.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<map>
+#include<tuple>
+#include<set>
+#include<cmath>
+#include<numeric>
+#define ll long long
+#define MOD = 1e9+7
+const double PI=3.14159265358979323846; 
+using namespace std;
+
+int main(){
+  cout << 1 << endl;
+}
